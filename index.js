25adb8400246fe597fb925edd82828a31a95a12a
Kevin Molid <kevinmolid@gmail.com>
2024-01-24 17:34:07 +0100

    Setup React (simple way with CDNs)

---
+++ index.js
@@ -0,0 +1,10 @@
+const root = document.getElementById("root")
+
+const unorderedList = (
+    <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+    </ul>
+)
+
+ReactDOM.render(unorderedList, root)
